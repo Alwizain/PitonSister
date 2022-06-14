50dff36f63410e6241d93c5788c9548fbdd586f8
FaisalAbdulkodir <faisalabdullah168@gmail.com>
2022-06-14 19:16:52 +0700

    chap5

--- Chapter05/concurrent_futures_pooling.py
+++ Chapter05/concurrent_futures_pooling.py
@@ -1,38 +1,42 @@
 import concurrent.futures
 import time
 
+
+time.clock = time.time
 number_list = list(range(1, 11))
 
 
-def count(number):
-    for i in range(0,10000000):
+def hitung(number):
+    for i in range(10000000):
         i += 1
     return i*number
 
 
-def evaluate(item):
-    result_item = count(item)
-    print('Item %s, result %s' % (item, result_item))
+def hasil(barang):
+    result_barang = hitung(barang)
+    print('Jika %s maka harganya %s' % (barang, result_barang))
 
 if __name__ == '__main__':
     # Sequential Execution
     start_time = time.clock()
-    for item in number_list:
-        evaluate(item)
-    print('Sequential Execution in %s seconds' % (time.clock() - start_time))
+    for i in number_list:
+        hasil(i)
+    print('Sequential Execution = %s seconds' % (time.clock() - start_time))
+    print("")
 
    
     # Thread Pool Execution
     start_time = time.clock()
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        for item in number_list:
-            executor.submit(evaluate, item)
-    print('Thread Pool Execution in %s seconds' % (time.clock() - start_time))
+        for i in number_list:
+            executor.submit(hasil, i)
+    print('Thread Pool Execution = %s seconds' % (time.clock() - start_time))
+    print("")
 
       
     # Process Pool Execution
     start_time = time.clock()
     with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
-        for item in number_list:
-            executor.submit(evaluate, item)
-    print('Process Pool Execution in %s seconds' % (time.clock() - start_time))
+        for i in number_list:
+            executor.submit(hasil, i)
+    print('Process Pool Execution = %s seconds' % (time.clock() - start_time))
